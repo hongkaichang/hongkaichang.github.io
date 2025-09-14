c5e82168604bcac5942d482a6fd9a66eecdb3e24
hongkaichang <948518741@qq.com>
2025-09-14 21:29:01 +0100

    Site updated: 2025-09-14 21:28:56

---
+++ js/ali_font.js
@@ -0,0 +1,19 @@
+@font-face {
+  font-family: "iconfont"; /* Project id 5017660 */
+  src: url('//at.alicdn.com/t/c/font_5017660_mvr44cis7us.woff2?t=1757836131160') format('woff2'),
+       url('//at.alicdn.com/t/c/font_5017660_mvr44cis7us.woff?t=1757836131160') format('woff'),
+       url('//at.alicdn.com/t/c/font_5017660_mvr44cis7us.ttf?t=1757836131160') format('truetype');
+}
+
+.iconfont {
+  font-family: "iconfont" !important;
+  font-size: 16px;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.icon-bangbangtang:before {
+  content: "\e6c7";
+}
+
